f1397bd9b492a9391bbcc12b6ed4cb062f9d6554
Denilson Arruda da Silva <denilsonarruda@outlook.com>
2023-04-05 14:47:21 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
 
-export default function App() {
+export default App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts/51"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const formatData = (data) => {
+    if (data) {
+      const jsonStr = JSON.stringify(data, null, 2);
+      return jsonStr
+        .split("\n")
+        .map((item, key) => <Text key={key}>{item}</Text>);
+    } else {
+      return <Text>Carregando...</Text>;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "flex-start",
+        backgroundColor: "#f2f2f2",
+      }}
+    >
+      {formatData(data)}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
